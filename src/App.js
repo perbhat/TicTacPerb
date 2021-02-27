de302e9f3ad7f56f30fe215ee4ffc893d03da2e2
Perbhat <pk524@njit.edu>
2021-02-27 21:02:16 +0000

    Added win functionality and permissinos for players and spectators

--- src/App.js
+++ src/App.js
@@ -8,27 +8,36 @@ import io from 'socket.io-client';
 const socket = io();
 
 function App() {
+  
+  const [thisUser, updateUser] = useState('')
+  
   const [userMap, updateUsers] = useState({
     playerX: '',
     playerO: '',
     spectators: []
   });
-  const inputUser = useRef(null);
+  const inputUser = useRef('');
+
   
   function onButtonClick(){
     var copy = {...userMap}
     const user = inputUser.current.value
     if(copy.playerX == ''){
       copy.playerX = user
+      updateUser(oldUser => 'X')
     }
     else if(copy.playerO == ''){
       copy.playerO = user
+       updateUser(oldUser => 'O')
     }
     else{
       let specs = [...copy.spectators, user]
       copy.spectators = specs
+      updateUser(oldUser => 's')
     }
     updateUsers(copy);
+    
+    
     
     socket.emit('login', {users: copy});
     console.log(copy);
@@ -43,17 +52,23 @@ function App() {
         console.log(data.users.spectators)
         });
      }, []);
-
+// If inputUser = playerX name then playerX boolean is True. Pass xTrue to board and if xTrue then only X, if xFalse, then only O. Then have a bool for can Turn after each time the array is updated. Opposites in the socket method maybe
   
-
+  
   
   return (
 
     
     <div>
+      <h3>PlayerX: {userMap.playerX}</h3>
+      <h3>PlayerO: {userMap.playerO}</h3>
+      <h3>Spectators:</h3>
+      {userMap.spectators.map((item) => <h3>{item}</h3>)}
+      
       <input type='text' ref={inputUser}/>
       <button onClick={onButtonClick}>Set the user for this tab </button>
-      <Board/>
+      <Board player={thisUser}/>
+      
     </div>
     
  
--- src/Board.js
+++ src/Board.js
@@ -7,46 +7,100 @@ import { Box } from './Box.js'
 
 const socket = io();
 
-export function Board() {
+export function Board(props) {
     
     const [boardState, changeState] = useState(Array(9).fill(null));
     
     const[nextVal, changeNext] = useState(true);
     
+
+    
+    
+    
     
     function onClickDiv(idx){
+        if(props.player == 's' || props.player == ''){
+            return
+        }
+
         const tempArr = [...boardState]
         const newVal = !nextVal
         if(boardState[idx] == null){
-            tempArr[idx] = nextVal ? 'X' : 'O'
+            tempArr[idx] = props.player == 'X' ? 'X' : 'O'
             changeNext(prevVal => newVal)
             changeState(prevList => tempArr)
         }
         socket.emit('turn', { board: tempArr, moveBool: newVal});
         console.log("Move Sent")
+   
         
     }
         
     useEffect(() => {
         socket.on('turn', (data) => {
-            console.log('Move Received')
-            console.log(data)
+            // console.log('Move Received')
+            // console.log(data)
             const currentState = data.board
             const moveState = data.moveBool
-            console.log(currentState)
+            // console.log(currentState)
             changeNext(prevVal => moveState)
             changeState(prevList => currentState)
+
             });
          }, []);
-        
+         
+    function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+    }
     
-    return (
-    <div class="board">
+    function boardIsFull(){
+        return boardState.every(element => element !== null)
         
-        {boardState.map((item, index) => <Box value={item} onClick={()=>onClickDiv(index)}/>)}
-
-    </div>
-    );
+    }
+    
+    if(calculateWinner(boardState) != null){
+        return (
+            
+            <h1> {calculateWinner(boardState)} WINS </h1>
+            
+            )
+    }
+    
+    else if(boardIsFull()){
+        return (
+            
+            <h1> DRAW </h1>
+            
+            )
+    }
+            
+    else{
+        return (
+            <>
+        <div class="board">
+            
+            {boardState.map((item, index) => <Box value={item} onClick={()=>onClickDiv(index)}/>)}
+    
+        </div>
+        </>
+        );
+    }
 }
 
 
